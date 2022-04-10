711f98c4940e52b3e4f51b609dd8606369bee996
Carlos Perez <pcarlos132@yahoo.com>
2022-04-10 16:43:16 -0500

    test

--- codewars_04-10-22/app.js
+++ codewars_04-10-22/app.js
@@ -16,3 +16,4 @@ function likes(names) {
     if(names.length === 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`;
     if(names.length >= 4) return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
   }
+  //test
