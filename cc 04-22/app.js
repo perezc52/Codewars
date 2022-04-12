9e7049c020a250f07872ceaceb7c69af17506cc8
Carlos Perez <pcarlos132@yahoo.com>
2022-04-12 14:40:55 -0500

    did codewars april 12 2022

--- cc 04-22/app.js
+++ cc 04-22/app.js
@@ -115,3 +115,27 @@ function findOdd(A) {
   const result = Object.keys(nums).filter(num => nums[num] % 2 === 1);
   return Number(result[0]);
 }
+
+//April 12 2022
+// Digital root is the recursive sum of all the digits in a number.
+
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+function digital_root(num) {
+  
+  let numArr = num.toString().split('');
+
+  let sum = numArr.map(num => Number(num)).reduce((acc, num) => acc + num);
+
+  while(!(sum < 10)) {
+    sum = sum.toString().split('').map(num => Number(num)).reduce((acc, num) => acc + num);
+  }
+  
+  return sum;
+  }
