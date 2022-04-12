ed4efe1cdbc42f07fedc475722da91589a9e89e0
Carlos Perez <pcarlos132@yahoo.com>
2022-04-11 19:34:56 -0500

    did codewars for april 11 2022

--- cc 04-22/app.js
+++ cc 04-22/app.js
@@ -68,3 +68,50 @@ function highestRank(arr){
 function createPhoneNumber(numbers){
   return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`;
 }
+
+//April 10 2022
+// You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+// Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+// Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+function likes(names) {
+  if(names.length === 0) return "no one likes this";
+  if(names.length === 1) return `${names[0]} likes this`;
+  if(names.length === 2) return `${names[0]} and ${names[1]} like this`;
+  if(names.length === 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  if(names.length >= 4) return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+}
+
+//April 11 2022
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+function findOdd(A) {
+  let nums = {};
+  
+  for(let i = 0; i < A.length; i++) {
+    if(A[i] in nums) {
+      nums[A[i]]++;
+    }else {
+      nums[A[i]] = 1;
+    }
+  }
+  
+  const result = Object.keys(nums).filter(num => nums[num] % 2 === 1);
+  return Number(result[0]);
+}
