8a14f470e649adabff31780e6f5d1e2d57a02988
Carlos Perez <pcarlos132@yahoo.com>
2022-04-13 18:59:42 -0500

    did codewars april 13 2022

--- cc 04-22/app.js
+++ cc 04-22/app.js
@@ -139,3 +139,20 @@ function digital_root(num) {
   
   return sum;
   }
+
+  //April 13 2022
+//   Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+// Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
+
+function spinWords(string){
+  let wordsArr = string.split(' ');
+  
+  for(let i = 0; i < wordsArr.length; i++) {
+    if(wordsArr[i].length >= 5){
+     wordsArr[i] = wordsArr[i].split('').reverse().join('');
+    }
+  }
+  
+  return wordsArr.join(' ');
+}
