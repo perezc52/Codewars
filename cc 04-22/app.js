62078d5bc5757e95dc11159cc4cae1c60a0701a1
Carlos Perez <pcarlos132@yahoo.com>
2022-04-14 16:23:09 -0500

    did codewars april 14 2022

--- cc 04-22/app.js
+++ cc 04-22/app.js
@@ -156,3 +156,25 @@ function spinWords(string){
   
   return wordsArr.join(' ');
 }
+
+//April 14 2022
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b keeping their order.
+
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+
+function arrayDiff(a, b) {
+  
+  for(let i = 0; i < b.length; i++) {
+    for(let j = 0; j < a.length; j++) {
+      if(a[j] === b[i]) a[j] = 'x';
+    }
+  }
+  
+  return a.filter(num => typeof num === 'number');
+}
