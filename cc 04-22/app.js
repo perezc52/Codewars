27d6578d34c69bb661f73b4c850cf5e1ccbe99aa
Carlos Perez <pcarlos132@yahoo.com>
2022-04-15 16:57:58 -0500

    did codewars april 15 2022

--- cc 04-22/app.js
+++ cc 04-22/app.js
@@ -178,3 +178,13 @@ function arrayDiff(a, b) {
   
   return a.filter(num => typeof num === 'number');
 }
+
+//April 15 2022
+// Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+// Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+var countBits = function(n) {
+  let nBinary = n.toString(2);
+  return nBinary.split('').filter(num => num == 1).length;
+};
