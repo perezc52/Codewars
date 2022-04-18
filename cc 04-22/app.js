30cdb21156d69872722bb2707183888372d09d6f
Carlos Perez <pcarlos132@yahoo.com>
2022-04-17 20:17:32 -0500

    did codewars april 17 2022

--- cc 04-22/app.js
+++ cc 04-22/app.js
@@ -211,3 +211,29 @@ function findOutlier(integers){
   }
   return evens.length === 1 ?  evens[0] : odds[0];
 }
+
+//April 17 2022
+// Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+function duplicateCount(text){
+  let textArr = text.split('').map(el => el.toLowerCase());
+  let textObj = {};
+  for(let i = 0; i < textArr.length; i++){
+    if(textArr[i] in textObj){
+      textObj[textArr[i]]++;
+    }else{
+      textObj[textArr[i]] = 1;
+    }
+  }
+  return Object.values(textObj).filter(el => el > 1).length;
+}
