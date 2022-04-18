3cc55c5e94b5afd69da55152847e9d8713ebf684
Carlos Perez <pcarlos132@yahoo.com>
2022-04-18 18:11:48 -0500

    did codewars april 18 2022

--- cc 04-22/app.js
+++ cc 04-22/app.js
@@ -237,3 +237,36 @@ function duplicateCount(text){
   }
   return Object.values(textObj).filter(el => el > 1).length;
 }
+
+//April 18 2022
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
+// Notes
+// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+function duplicateEncode(text){
+  let textArr = text.toLowerCase().split('');
+  let textObj = {};
+  for(let i = 0; i < textArr.length; i++){
+    if(textArr[i] in textObj){
+      textObj[textArr[i]]++;
+    }else{
+      textObj[textArr[i]] = 1;
+    }
+  }
+
+  for(let j = 0; j < textArr.length; j++) {
+    if(textObj[textArr[j]] == 1) {
+      textArr[j] = '(';
+    }else {
+      textArr[j] = ')';
+    }
+  }
+
+  return textArr.join('');
+}
