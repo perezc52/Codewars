fdbc16566bb9962353b7eba0bae29ee4388f1db3
Carlos Perez <pcarlos132@yahoo.com>
2022-04-20 19:17:00 -0500

    april 20 2022

--- cc 04-22/app.js
+++ cc 04-22/app.js
@@ -290,3 +290,14 @@ function isValidWalk(walk) {
 
   return walk.length == 10 && sum == 0 ? true : false;
 }
+
+//April 20 2022
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str){
+  return str.split('').reverse().join('');
+}
+
