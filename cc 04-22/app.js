a8a2703bc2b0cde783dfebd0b2cc764cb29c30c6
Carlos Perez <pcarlos132@yahoo.com>
2022-04-22 16:11:42 -0500

    april 22 2022

--- cc 04-22/app.js
+++ cc 04-22/app.js
@@ -330,3 +330,31 @@ function multiply(num) {
   return result;
 }
 
+//April 22 2022
+// In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+// If anything in the text isn't a letter, ignore it and don't return it.
+
+// "a" = 1, "b" = 2, etc.
+
+// Example
+// alphabetPosition("The sunset sets at twelve o' clock.")
+// Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
+
+function alphabetPosition(text) {
+  text = text.toLowerCase();
+  
+  let alphabet = new Map();
+  let arr = [];
+
+  for(let i = 0; i < 26; i++) {
+    alphabet.set((i+10).toString(36), i+1);
+  }
+  
+  for(let i = 0; i < text.length; i++){
+    if(text[i] = alphabet.get(text[i])) arr.push(alphabet.get(text[i]));
+  }
+  
+  return arr.join(' ');
+}
+
