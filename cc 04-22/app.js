1a6c4c39b733b1f06588513e0fcc018ceb8cce82
Carlos Perez <pcarlos132@yahoo.com>
2022-04-25 16:45:51 -0500

    april 25 2022

--- cc 04-22/app.js
+++ cc 04-22/app.js
@@ -375,3 +375,15 @@ function isDivisible(n, x, y) {
   }
 }
 
+//April 25 2022
+// Given an integral number, determine if it's a square number:
+
+// In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+// The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
+var isSquare = function(n){
+  let root = Math.sqrt(n);
+  return root % 1 === 0 ? true : false;
+}
+
