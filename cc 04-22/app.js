9a4f3c40367ca8a52f07c2dd37195f6aa36afe94
Carlos Perez <pcarlos132@yahoo.com>
2022-04-26 16:50:45 -0500

    april 26 2022

--- cc 04-22/app.js
+++ cc 04-22/app.js
@@ -387,3 +387,20 @@ var isSquare = function(n){
   return root % 1 === 0 ? true : false;
 }
 
+//April 26 2022
+// Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+// "found the needle at position " plus the index it found the needle
+
+function findNeedle(haystack) {
+  for(let i = 0; i < haystack.length; i++) {
+    if(haystack[i] == 'needle') {
+      return `found the needle at position ${i}`;
+    }
+  }
+}
+
