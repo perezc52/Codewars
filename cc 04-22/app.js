d5fa1ab19187f999e5104745daaba059d975a16c
Carlos Perez <pcarlos132@yahoo.com>
2022-04-27 18:30:11 -0500

    april 27 2022

--- cc 04-22/app.js
+++ cc 04-22/app.js
@@ -404,3 +404,28 @@ function findNeedle(haystack) {
   }
 }
 
+//April 27 2022
+// Write a generic function chainer
+// Write a generic function chainer that takes a starting value, and an array of functions to execute on it (array of symbols for Ruby).
+
+// The input for each function is the output of the previous function (except the first function, which takes the starting value as its input). Return the final value after execution is complete.
+
+// function add(num) {
+//   return num + 1;
+// }
+
+// function mult(num) {
+//   return num * 30;
+// }
+
+// chain(2, [add, mult]);
+// // returns 90;
+
+function chain(input, fs) {
+  for(let i = 0; i < fs.length; i++) {
+    console.log(fs[i](input));
+    input = fs[i](input);
+  }
+  return input;
+}
+
