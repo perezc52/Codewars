4a7ada4cfe735ed89bb3d9224edacce21e7eefef
Carlos Perez <pcarlos132@yahoo.com>
2022-04-28 17:10:00 -0500

    april 28 2022

--- cc 04-22/app.js
+++ cc 04-22/app.js
@@ -429,3 +429,20 @@ function chain(input, fs) {
   return input;
 }
 
+//April 28
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+function countPositivesSumNegatives(input) {
+  if(!Array.isArray(input) || !input.length) return [];
+  let countPositives = input.filter(el => el > 0).length;
+  let sumNegatives = input.filter(el => el < 0).reduce((acc, c) => acc + c, 0);
+  return [countPositives, sumNegatives];
+}
+
