be4ff7d260995cc199d968e378727ec0136437a3
Carlos Perez <pcarlos132@yahoo.com>
2022-04-29 19:05:56 -0500

    april 29 2022

--- cc 04-22/app.js
+++ cc 04-22/app.js
@@ -446,3 +446,33 @@ function countPositivesSumNegatives(input) {
   return [countPositives, sumNegatives];
 }
 
+//April 29
+// I have a cat and a dog.
+
+// I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+// Return their respective ages now as [humanYears,catYears,dogYears]
+
+// NOTES:
+
+// humanYears >= 1
+// humanYears are whole numbers only
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+  let catYears = 0;
+  let dogYears = 0;
+  for(let i = 1; i <= humanYears; i++) {
+      if(i === 1) {
+        catYears += 15;
+        dogYears += 15;
+      }else if(i === 2) {
+        catYears += 9;
+        dogYears += 9;
+      }else {
+        catYears += 4;
+        dogYears += 5;
+      }
+  }
+  return [humanYears, catYears, dogYears];
+}
+
