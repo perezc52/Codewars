3c4027bb58974ab4f13c0abdcbaf564ce9b3955e
Carlos Perez <pcarlos132@yahoo.com>
2022-04-30 19:58:57 -0500

    april 30 2022

--- cc 04-22/app.js
+++ cc 04-22/app.js
@@ -476,3 +476,22 @@ var humanYearsCatYearsDogYears = function(humanYears) {
   return [humanYears, catYears, dogYears];
 }
 
+//April 30
+// Write a method, that will get an integer array as parameter and will process every number from this array.
+
+// Return a new array with processing every number of the input-array like this:
+
+// If the number has an integer square root, take this, otherwise square the number.
+
+// Example
+// [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+function squareOrSquareRoot(array) {
+  return array.map(el => {
+    if(Math.sqrt(el) % 1 === 0) {
+      return Math.sqrt(el);
+    }else{
+      return el**2;
+    }
+  });
+}
+
