f1fc83295dc2ab79b30be200d4c7d93a65008fc3
Carlos Perez <pcarlos132@yahoo.com>
2022-05-02 19:32:50 -0500

    may 2 2022

--- May 2022/main.js
+++ May 2022/main.js
@@ -24,3 +24,13 @@ function sameCase(a, b){
       return 0;
     }
     }
+
+    //May 2 2022
+//     Write a function that returns a string in which firstname is swapped with last name.
+
+// nameShuffler('john McClane'); => "McClane john"
+
+function nameShuffler(str){
+  return str.split(' ').reverse().join(' ');
+  
+}
