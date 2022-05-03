09db0d050dca41b45ba591afbc175fb0b0b730ca
Carlos Perez <pcarlos132@yahoo.com>
2022-05-03 16:27:06 -0500

    may 3 2022

--- May 2022/main.js
+++ May 2022/main.js
@@ -34,3 +34,35 @@ function nameShuffler(str){
   return str.split(' ').reverse().join(' ');
   
 }
+
+//May 3 2022
+// The BOPE is the squad of special forces of police that usually handles the operations in the Favelas in Rio de Janeiro.
+
+// In this Kata you have to write a function that determine the number of magazines that every soldier has to have in his bag.
+
+// You will receive the weapon and the number of streets that they have to cross. Considering that every street the officer shoots 3 times. Bellow there is the relation of weapons:
+
+// PT92 - 17 bullets
+// M4A1 - 30 bullets
+// M16A2 - 30 bullets
+// PSG1 - 5 bullets
+
+// Example:
+
+// ["PT92", 6] => 2 (6 streets 3 bullets each)
+// ["M4A1", 6] => 1
+
+// The return Will always be an integer so as the params.
+
+function magNumber(info){
+  const totalBullets = info[1] * 3;
+  if(info[0] === 'PT92') {
+    return Math.ceil(totalBullets / 17);
+  }else if(info[0] === 'M4A1') {
+    return Math.ceil(totalBullets / 30);
+  }else if(info[0] === 'M16A2') {
+   return Math.ceil(totalBullets / 30);
+  }else {
+    return Math.ceil(totalBullets / 5);
+  }
+}
