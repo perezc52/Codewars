4f6e81c488976a02c8c28c64a74379f820b58044
Carlos Perez <pcarlos132@yahoo.com>
2022-05-09 14:16:54 -0500

    may 9 2022

--- May 2022/main.js
+++ May 2022/main.js
@@ -140,3 +140,23 @@ function lovefunc(flower1, flower2){
     return false;
   }
 }
+
+//May 9 2022
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+// For example:
+
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+var uniqueInOrder=function(iterable){
+  let unique = [];
+  for(let i = 0; i < iterable.length; i++) {
+    if(iterable[i] !== iterable[i-1]) {
+      unique.push(iterable[i]);
+    }
+  }
+  console.log(unique)
+  return unique
+}
