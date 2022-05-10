6b78381b10c1cb196316a325e2604bd8cc885eb7
Carlos Perez <pcarlos132@yahoo.com>
2022-05-10 17:23:02 -0500

    may 10 2022

--- May 2022/main.js
+++ May 2022/main.js
@@ -160,3 +160,25 @@ var uniqueInOrder=function(iterable){
   console.log(unique)
   return unique
 }
+
+//May 10 2022
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+// Examples
+// pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+// pigIt('Hello world !');     // elloHay orldway !
+
+function pigIt(str){
+  str = str.split(' ');
+  let latinStr = [];
+  for(let word of str) {
+    if(word === '!' || word === '.' || word === ',' || word === '?') {
+      latinStr.push(word);
+      continue;
+    }
+    let firstLetter = word[0];
+    word = word.slice(1) + firstLetter + 'ay';
+    latinStr.push(word);
+  }
+  return latinStr.join(' ');
+}
