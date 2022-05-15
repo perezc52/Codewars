4b5209094709a1face387ba4d4ff5a115489d89a
Carlos Perez <pcarlos132@yahoo.com>
2022-05-15 13:08:26 -0500

    may 15 2022

--- May 2022/main.js
+++ May 2022/main.js
@@ -270,3 +270,25 @@ function isPangram(string){
   }
   return alphabet.size === 0 ? true : false;
 }
+
+//May 15 2022
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(s) {
+  s = s.toUpperCase();
+	let result = "";
+  for(let i = 0; i < s.length; i++) {
+    if(i === s.length - 1) {
+      result += s[i] + s[i].toLowerCase().repeat(i);
+    }else {
+    result += s[i] + s[i].toLowerCase().repeat(i) + '-';
+      }
+  }
+  return result;
+}
