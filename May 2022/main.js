4889254d23dbbd47f28cf806e382251a642ccff0
Carlos Perez <pcarlos132@yahoo.com>
2022-05-31 15:57:53 -0500

    may 31 2022

--- May 2022/main.js
+++ May 2022/main.js
@@ -608,3 +608,27 @@ var number = function(busStops){
   // Good Luck!
   return busStops.map(el => el[0] - el[1]).reduce((acc, el) => acc + el, 0)
 }
+
+//May 31 2022
+Task:
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Examples:
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
+
+function oddOrEven(array) {
+  //enter code here
+ if(array === [] || array === [0]) return 'even'
+ return array.reduce((acc, el) => acc + el, 0) % 2 === 0 ? 'even' : 'odd'
+}
