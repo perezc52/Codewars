ec1db0cacb6449ff7d12e0954f8044019082ac58
Carlos Perez <pcarlos132@yahoo.com>
2022-06-06 19:02:35 -0500

    june 6 2022

--- June 2022/main.js
+++ June 2022/main.js
@@ -104,3 +104,22 @@ function minMax(arr){
   let max = Math.max(...arr)
   return [min, max]
 }
+
+//June 6 2022
+// Count the number of divisors of a positive integer n.
+
+// Random tests go up to n = 500000.
+
+// Examples (input --> output)
+// 4 --> 3 (1, 2, 4)
+// 5 --> 2 (1, 5)
+// 12 --> 6 (1, 2, 3, 4, 6, 12)
+// 30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+
+function getDivisorsCnt(n){
+  let divs = []
+  for(let i = 1; i <= n; i++) {
+    if(n % i === 0) divs.push(i)
+  }
+return divs.length
+}
