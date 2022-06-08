ea42b82ef5a29e50b357c319989effc42af1aee5
Carlos Perez <pcarlos132@yahoo.com>
2022-06-08 16:35:30 -0500

    june 8 2022

--- June 2022/main.js
+++ June 2022/main.js
@@ -134,3 +134,24 @@ return divs.length
 function breakChocolate(n,m) {
   return (n * m)  - 1 > 0 ? (n * m) - 1 : 0
 }
+
+//June 8 2022
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+
+// The tests contain some very huge arrays, so think about performance.
+
+function findUniq(arr) {
+  let counts = {}
+  for(let num of arr) {
+    if(counts[num]) {
+      counts[num]++
+    }else {
+      counts[num] = 1
+    }
+  }
+  return Number(Object.keys(counts).filter(key => counts[key] === 1))
+}
