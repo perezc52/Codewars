57e8e01737660b1446faf6600ca6acdeee6a44b1
Carlos Perez <pcarlos132@yahoo.com>
2022-06-12 21:46:16 -0500

    june 12 2022

--- June 2022/main.js
+++ June 2022/main.js
@@ -227,3 +227,23 @@ function dontGiveMeFive(start, end) {
   }
   return count
 }
+
+//June 12 2022
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+// Mind the input validation.
+
+// Example
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+
+function sumArray(array) {
+  if(!array) return 0
+  if(array.length < 3) return 0
+  let sorted = array.sort((a, b) => a - b)
+  sorted.pop()
+  sorted.shift()
+  return sorted.reduce((acc, el) => acc + el, 0)
+}
