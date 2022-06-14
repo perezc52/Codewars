e4c4e9ae7cb141ec51c6adf05004cfa2b99e8ece
Carlos Perez <pcarlos132@yahoo.com>
2022-06-13 20:00:40 -0500

    june 13 2022

--- June 2022/main.js
+++ June 2022/main.js
@@ -247,3 +247,21 @@ function sumArray(array) {
   sorted.shift()
   return sorted.reduce((acc, el) => acc + el, 0)
 }
+
+//June 13 2022
+// You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+// Example(Input1, Input2 --> Output):
+
+// 6, 10 --> 32
+// 3, 3 --> 9
+// Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+
+const areaOrPerimeter = function(l , w) {
+  if(l === w) {
+    return l * w
+  } else {
+    return (2 * l) + (2 * w)
+  }
+};
