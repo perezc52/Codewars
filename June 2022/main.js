43af9879142e775fa9700ca1f0a70a15cfbef6eb
Carlos Perez <pcarlos132@yahoo.com>
2022-06-16 17:00:49 -0500

    june 16 2022

--- June 2022/main.js
+++ June 2022/main.js
@@ -307,3 +307,17 @@ function arithmetic(a, b, operator){
 function isDivideBy(number, a, b) {
   return number % a === 0 && number % b === 0 ? true : false
 }
+
+//June 16 2022
+// In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
+
+// Write a function to calculate factorial for a given input. If input is below 0 or above 12 throw an exception of type ArgumentOutOfRangeException (C#) or IllegalArgumentException (Java) or RangeException (PHP) or throw a RangeError (JavaScript) or ValueError (Python) or return -1 (C).
+
+function factorial(n) {
+  if(n < 0 || n > 12) throw 'RangeError'
+  let num = 1
+  for(let i = 1; i <= n; i++) {
+    num *= i
+  }
+  return num
+}
