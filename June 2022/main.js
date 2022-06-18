bc7a7cde9c82bfca95ad8df5b38802ef8c93228d
Carlos Perez <pcarlos132@yahoo.com>
2022-06-18 18:19:27 -0500

    june 18 2022

--- June 2022/main.js
+++ June 2022/main.js
@@ -344,3 +344,25 @@ const sequenceSum = (begin, end, step) => {
   }
   return sum
 };
+
+//June 18 2022
+// In the following 6 digit number:
+
+// 283910
+// 91 is the greatest sequence of 2 consecutive digits.
+
+// In the following 10 digit number:
+
+// 1234567890
+// 67890 is the greatest sequence of 5 consecutive digits.
+
+// Complete the solution so that it returns the greatest sequence of five consecutive digits found within the number given. The number will be passed in as a string of only digits. It should return a five digit integer. The number passed may be as large as 1000 digits.
+
+function solution(digits){
+  let digitS = digits.toString()
+  let nums = []
+  for(let i = 0; i < digits.length; i++) {
+    nums.push(digitS.slice(i, i+5))
+  }
+  return Math.max(...nums)
+}
