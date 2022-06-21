cde114c3fd0e076843341cb4d7c2876fda2af02a
Carlos Perez <pcarlos132@yahoo.com>
2022-06-21 17:15:43 -0500

    june 21 2022

--- June 2022/main.js
+++ June 2022/main.js
@@ -398,3 +398,28 @@ function twoOldestAges(ages){
   let secondHighest = Math.max(...newArray)
   return [secondHighest, highest]
 }
+
+//June 21 2022
+Task
+// Given an array of numbers and an index, return either the index of the smallest number that is larger than the element at the given index, or -1 if there is no such index ( or, where applicable, Nothing or a similarly empty value ).
+
+// Notes
+// Multiple correct answers may be possible. In this case, return any one of them.
+// The given index will be inside the given array.
+// The given array will, therefore, never be empty.
+
+// Example
+// leastLarger( [4, 1, 3, 5, 6], 0 )  =>  3
+// leastLarger( [4, 1, 3, 5, 6], 4 )  => -1
+
+function leastLarger(arr,index) {
+  let possible = []
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] > arr[index]) possible.push(arr[i])
+  }
+  if(possible.length === 0){
+    return -1
+  }else {
+    return arr.indexOf(Math.min(...possible))
+  }
+}
