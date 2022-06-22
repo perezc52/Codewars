c973c63cb45be37e5497988b037e6ce5ea3957a8
Carlos Perez <pcarlos132@yahoo.com>
2022-06-22 16:53:13 -0500

    june 22 2022

--- June 2022/main.js
+++ June 2022/main.js
@@ -423,3 +423,14 @@ function leastLarger(arr,index) {
     return arr.indexOf(Math.min(...possible))
   }
 }
+
+//June 22 2022
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+function removeUrlAnchor(url){  
+  return url.includes('#') ? url.slice(0, url.indexOf('#')) : url
+}
