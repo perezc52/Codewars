c9199f44150611a3301dc3e6c29fc2658f2cf39f
Carlos Perez <pcarlos132@yahoo.com>
2022-06-24 15:51:12 -0500

    june 24 2022

--- June 2022/main.js
+++ June 2022/main.js
@@ -457,3 +457,12 @@ function sortByLength (array) {
   })
   return result
 };
+
+//June 24 2022
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
+
+function noOdds( values ){
+  return values.filter(el => el % 2 === 0)
+}
