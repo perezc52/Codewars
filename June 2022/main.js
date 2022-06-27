251d1753e3bf0a50d3e8678457663d210c799eb9
Carlos Perez <pcarlos132@yahoo.com>
2022-06-26 20:25:00 -0500

    june 26 2022

--- June 2022/main.js
+++ June 2022/main.js
@@ -478,3 +478,36 @@ function expressionMatter(a, b, c) {
   let par2 = a * (b + c)
   return Math.max(sum, product, par1, par2)
 }
+
+//June 26 2022
+Story
+// YouTube had a like and a dislike button, which allowed users to express their opinions about particular content. It was set up in such a way that you cannot like and dislike a video at the same time. There are two other interesting rules to be noted about the interface: Pressing a button, which is already active, will undo your press. If you press the like button after pressing the dislike button, the like button overwrites the previous "Dislike" state. The same is true for the other way round.
+
+// Task
+// Create a function that takes in a list of button inputs and returns the final state.
+
+// Examples
+// likeOrDislike([Dislike]) => Dislike
+// likeOrDislike([Like,Like]) => Nothing
+// likeOrDislike([Dislike,Like]) => Like
+// likeOrDislike([Like,Dislike,Dislike]) => Nothing
+
+// Like, Dislike, Nothing come from Preloaded
+
+function likeOrDislike(buttons) {
+  let like = false
+  let dislike = false
+  for(let button of buttons) {
+    if(button === 'Like') {
+      like = !like
+      dislike = false
+    } else if(button === 'Dislike') {
+      dislike = !dislike
+      like = false
+    } 
+  }
+  if(like === true && dislike === false) return 'Like'
+  if(like === false && dislike === true) return 'Dislike'
+  if(like === false && dislike === false) return 'Nothing'
+  
+}
