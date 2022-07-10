d36c433d785e15fe7a4cad0c39c7999493ae0689
Carlos Perez <pcarlos132@yahoo.com>
2022-07-10 13:08:05 -0500

    july 10 2022

--- July 2022/main.js
+++ July 2022/main.js
@@ -82,3 +82,26 @@ function gimme (triplet) {
   let middle = triplet.slice().sort((a, b) => a - b)[1]
   return triplet.indexOf(middle)
 }
+
+//June 10 2022
+// Given an integer as input, can you round it to the next (meaning, "higher") multiple of 5?
+
+// Examples:
+
+// input:    output:
+// 0    ->   0
+// 2    ->   5
+// 3    ->   5
+// 12   ->   15
+// 21   ->   25
+// 30   ->   30
+// -2   ->   0
+// -5   ->   -5
+// etc.
+
+function roundToNext5(n){
+  while(n % 5 !== 0) {
+    n++
+  }
+  return n
+}
