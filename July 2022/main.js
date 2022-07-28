b546aed22c84fa6216437af4500195068f181e35
Carlos Perez <pcarlos132@yahoo.com>
2022-07-27 20:16:39 -0500

    july 27 2022

--- July 2022/main.js
+++ July 2022/main.js
@@ -150,3 +150,18 @@ function nearestSq(n){
     return higher
   }
 }
+
+//July 27 2022
+// The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+// But it's not working properly.
+
+// Task
+// Fix the bug so we can all go home early.
+
+// Notes
+// String str will never be null.
+
+var replaceDots = function(str) {
+  return str.replace(/[.]/g, '-');
+}
