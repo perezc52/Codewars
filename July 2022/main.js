638cd906e667e75b23bf4a0503ad63859730c520
Carlos Perez <pcarlos132@yahoo.com>
2022-07-31 17:14:50 -0500

    july 31 2022

--- July 2022/main.js
+++ July 2022/main.js
@@ -212,3 +212,14 @@ function removeRotten(bagOfFruits){
   })
   return arr
 }
+
+//July 31 2022
+// Your task is to write function factorial.
+
+function factorial(n){
+  if(n === 0) return 1
+  for(let i = n-1; i >= 1; i--) {
+    n *= i
+  }
+  return n
+}
