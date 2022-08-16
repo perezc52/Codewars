a4757fdf74c2a0a96f0e4b8bca44ec648839d164
Carlos Perez <pcarlos132@yahoo.com>
2022-08-16 14:08:56 -0500

    august 16 2022

--- April 2022/app.js
+++ April 2022/app.js
@@ -495,4 +495,27 @@ function squareOrSquareRoot(array) {
   });
 }
 
+//August 16 2022
+// Every book has n pages with page numbers 1 to n. The summary is made by adding up the number of digits of all page numbers.
+
+// Task: Given the summary, find the number of pages n the book has.
+
+// Example
+// If the input is summary=25, then the output must be n=17: The numbers 1 to 17 have 25 digits in total: 1234567891011121314151617.
+
+// Be aware that you'll get enormous books having up to 100.000 pages.
+
+// All inputs will be valid.
+
+function amountOfPages(summary){
+  let str = ''
+  let count = 1
+  
+  while(str.length < summary) {
+    str = str + String(count)
+    ++count
+  }
+  return count-1
+}
+
 
