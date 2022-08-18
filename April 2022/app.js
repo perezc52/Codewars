10d65a14e077987270696e481d9e3e35620d6a1b
Carlos Perez <pcarlos132@yahoo.com>
2022-08-18 16:17:34 -0500

    august 18 2022

--- April 2022/app.js
+++ April 2022/app.js
@@ -552,3 +552,28 @@ function getLengthOfMissingArray(arrayOfArrays) {
   return 0
 }
 
+//August 18 2022
+
+// Output
+// #Find the missing letter
+
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+// You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+// The array will always contain letters in only one case.
+
+// Example:
+
+// ['a','b','c','d','f'] -> 'e' ['O','Q','R','S'] -> 'P'
+
+// ["a","b","c","d","f"] -> "e"
+// ["O","Q","R","S"] -> "P"
+
+function findMissingLetter(array) {
+  let nums = array.map(el => el.charCodeAt())
+  
+  let missing
+  for(let i = nums[0]; i <= nums[nums.length-1]; i++) {
+    if(!nums.includes(i)) return String.fromCharCode(i)
+  }
+}
