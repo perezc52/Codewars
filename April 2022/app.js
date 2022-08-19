aa15b0c4e42ed922509d7cb4e5b46fd6e18d03d0
Carlos Perez <pcarlos132@yahoo.com>
2022-08-19 14:53:27 -0500

    august 19 2022

--- April 2022/app.js
+++ April 2022/app.js
@@ -577,3 +577,20 @@ function findMissingLetter(array) {
     if(!nums.includes(i)) return String.fromCharCode(i)
   }
 }
+
+//AUgust 19 2022
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+function count (string) {  
+  let result = {}
+  for(let i = 0; i < string.length; i++) {
+    if(result[string[i]]) {
+      result[string[i]] += 1
+    }else {
+      result[string[i]] = 1
+    }
+  }
+  return result
+}
