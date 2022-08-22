0228e73fb65fd7831f91f618b897f376a0104ddc
Carlos Perez <pcarlos132@yahoo.com>
2022-08-22 14:05:04 -0500

    august 22 2022

--- August 2022/app.js
+++ August 2022/app.js
@@ -240,3 +240,21 @@ function toWeirdCase(string){
           .join('')
   }
 }
+
+//August 22 2022
+// I will give you an integer. Give me back a shape that is as long and wide as the integer. The integer will be a whole number between 1 and 50.
+
+// Example
+// n = 3, so I expect a 3x3 square back just like below as a string:
+
+// +++
+// +++
+// +++
+
+function generateShape(integer){
+  let str = ''
+  for(let i = 0; i < integer-1; i++) {
+    str += `${"+".repeat(integer)}\n`
+  }
+  return str+`${"+".repeat(integer)}`
+}
