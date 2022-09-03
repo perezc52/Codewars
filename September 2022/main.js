39fd1a27ead1e8c4c4e645ec1f157f191c6b05da
Carlos Perez <pcarlos132@yahoo.com>
2022-09-02 20:14:51 -0500

    september 2 2022

--- September 2022/main.js
+++ September 2022/main.js
@@ -4,3 +4,10 @@
 Array.prototype.remove_ = function(integer_list, values_list){
     return integer_list.filter(el => !values_list.includes(el))
   }
+
+  //September 2 2022
+  // This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
