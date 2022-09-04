6b003fe0f6dd88dddc293d95f41d4f0b43b7a6b3
Carlos Perez <pcarlos132@yahoo.com>
2022-09-03 19:10:08 -0500

    september 3 2022

--- September 2022/main.js
+++ September 2022/main.js
@@ -11,3 +11,15 @@ Array.prototype.remove_ = function(integer_list, values_list){
 var a = "code";
 var b = "wa.rs";
 var name = a + b;
+
+//September 3 2022
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+function solution(str, ending){
+  return str.endsWith(ending)
+ }
