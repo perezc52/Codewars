0098721d933b724ba9c5fa26d4d9c9acf17970b4
Carlos Perez <pcarlos132@yahoo.com>
2022-09-07 18:33:10 -0500

    september 7 2022

--- September 2022/main.js
+++ September 2022/main.js
@@ -56,3 +56,15 @@ function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
 function toAcronym(inp){
   return inp.split(' ').map(el => el[0].toUpperCase()).join('')
  }
+
+ //September 7 2022
+//  You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
+
+// lst contains integers, that means it may contain some negative numbers
+// if lst is empty or contains a single element, return 0
+// lst is not sorted
+
+function maxDiff(list) {
+  if(list.length === 0) return 0
+  return Math.max(...list) - Math.min(...list)
+};
