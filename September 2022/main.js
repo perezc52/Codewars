50db8d8188c586240475ca8f53555d7d05e87bcb
Carlos Perez <pcarlos132@yahoo.com>
2022-09-13 17:19:11 -0500

    september 13 2022

--- September 2022/main.js
+++ September 2022/main.js
@@ -151,3 +151,37 @@ function scoreTest(str, right, omit, wrong){
 function spacify(str) {
   return str.split('').join(' ')
 }
+
+//September 13 2022
+// The year is 2088 and the Radical Marxist Socialist People's Party (RMSPP) has just seized power in Brazil.
+
+// Their first act in power is absolute wealth equality through coercive redistribution.
+
+// Create a function that redistributes all wealth equally among all citizens.
+
+// Wealth is represented as an array/list where every index is the wealth of a single citizen.
+// The function should mutate the input such that every index has the same amount of wealth.
+
+// See example:
+
+// Input:
+// [5, 10, 6]  >>> This represents:
+//            # citizen 1 has wealth 5
+//            # citizen 2 has wealth 10
+//            # citizen 3 has wealth 6
+// Should be after the test:
+//  [7, 7, 7] >>> wealth has now been equally redistributed
+// Info:
+
+// MUTATE the input array/list, don't return anything
+// Input is guaranteed to hold at least 1 citizen
+// Wealth of a citizen will be an integer with minimum equal to 0 (negative wealth is not possible)
+// Handling of floating point error will not be tested
+
+function redistributeWealth(wealth) {
+  let average = wealth.reduce((acc, el) => acc + el) / wealth.length
+  for(let i = 0; i < wealth.length; i++) {
+    wealth.splice(i, 1, average)
+  }
+  void wealth
+}
