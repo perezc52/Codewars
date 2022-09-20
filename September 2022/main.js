a0c1c2f69ed8d474a322937406efc499e8e3f2e0
Carlos Perez <pcarlos132@yahoo.com>
2022-09-19 19:08:39 -0500

    september 19 2022

--- September 2022/main.js
+++ September 2022/main.js
@@ -262,3 +262,17 @@ function tidyNumber(n){
 function flattenAndSort(array) {
   return array.flat().sort((a, b) => a - b)
 }
+
+//September 19 2022
+// Given an array of numbers (in string format), you must return a string. The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
+
+// All inputs will be valid.
+
+function switcher(x){
+  return x.map(el => {
+    if(el == 27) return '!'
+    if(el == 28) return '?'
+    if(el == 29) return ' '
+    return String.fromCharCode(123-el)
+  }).join('')
+}
