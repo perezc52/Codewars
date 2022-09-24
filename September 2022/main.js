8c37d597c9f01064730caa041d16948874a4cf42
Carlos Perez <pcarlos132@yahoo.com>
2022-09-24 17:33:30 -0500

    september 24 2022

--- September 2022/main.js
+++ September 2022/main.js
@@ -330,3 +330,20 @@ function findDeletedNumber(arr, mixArr) {
     if(mixArr[i] !== arr[i]) return arr[i]
   }
 }
+
+//September 24 2022
+// Given a 2D ( nested ) list ( array, vector, .. ) of size m * n, your task is to find the sum of the minimum values in each row.
+
+// For Example:
+
+// [ [ 1, 2, 3, 4, 5 ]        #  minimum value of row is 1
+// , [ 5, 6, 7, 8, 9 ]        #  minimum value of row is 5
+// , [ 20, 21, 34, 56, 100 ]  #  minimum value of row is 20
+// ]
+// So the function should return 26 because the sum of the minimums is 1 + 5 + 20 = 26.
+
+// Note: You will always be given a non-empty list containing positive values.
+
+function sumOfMinimums(arr) {
+  return arr.map(el => Math.min(...el)).reduce((acc, el) => acc + el)
+}
