9f0beb4c6be11ab3698577f193ceb0c9ed91b907
Carlos Perez <pcarlos132@yahoo.com>
2022-09-25 17:13:52 -0500

    september 25 2022

--- September 2022/main.js
+++ September 2022/main.js
@@ -347,3 +347,30 @@ function findDeletedNumber(arr, mixArr) {
 function sumOfMinimums(arr) {
   return arr.map(el => Math.min(...el)).reduce((acc, el) => acc + el)
 }
+
+//September 25 2022
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+// Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+function order(words){
+  let values = {}
+
+  let wordsArr = words.split(' ')
+  for(let i = 0; i < wordsArr.length; i++) {
+    let num = wordsArr[i].split('').filter(el => '0123456789'.includes(el))[0]
+    values[wordsArr[i]] = Number(num)
+  }
+
+  let sortedEntries = Object.entries(values).sort((a,b) => a[1] -b[1])
+
+  return sortedEntries.map(el => el[0]).join(' ')
+
+}
