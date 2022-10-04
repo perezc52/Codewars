fe6a2967fe1b8800eb0006034563cc46f456bcee
Carlos Perez <pcarlos132@yahoo.com>
2022-10-04 16:27:24 -0500

    october 4 2022

--- October 2022/main.js
+++ October 2022/main.js
@@ -60,3 +60,37 @@ function tribonacci(signature,n){
   }
   return res
 }
+
+//October 4 2022
+// Build Tower
+// Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+// For example, a tower with 3 floors looks like this:
+
+// [
+//   "  *  ",
+//   " *** ", 
+//   "*****"
+// ]
+// And a tower with 6 floors looks like this:
+
+// [
+//   "     *     ", 
+//   "    ***    ", 
+//   "   *****   ", 
+//   "  *******  ", 
+//   " ********* ", 
+//   "***********"
+// ]
+
+function towerBuilder(nFloors) {
+  var space = "";
+  var star = "";
+  var result = [];
+  for (var i = 1; i <= nFloors; i++) {
+    space = (" ").repeat(nFloors - i);
+    star = ("*").repeat((2 * i) - 1);
+    result.push(space + star + space);
+  }
+  return result;
+}
