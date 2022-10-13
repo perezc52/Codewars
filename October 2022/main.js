818680f833a9a9efd63758dc7ace5adc38978a0b
Carlos Perez <pcarlos132@yahoo.com>
2022-10-12 19:39:28 -0500

    october 12 2022

--- October 2022/main.js
+++ October 2022/main.js
@@ -94,3 +94,21 @@ function towerBuilder(nFloors) {
   }
   return result;
 }
+
+//October 12 2022
+// Given a number as an input, print out every integer from 1 to that number. However, when the integer is divisible by 3, print out “Fizz”; when it’s divisible by 5, print out “Buzz”; when it’s divisible by both 3 and 5, print out “Fizz Buzz”.
+
+function fizzbuzz(num) {
+  for(let i = 1; i <= num; i++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+      console.log('Fizz Buzz')
+    }else if(i % 5 === 0) {
+      console.log('Buzz')
+    }else if(i % 3 === 0) {
+      console.log('Fizz')
+    }else {
+      console.log(i)
+    }
+  }
+}
+console.log(fizzbuzz(15))
