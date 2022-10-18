df5f5640effb34e24bab4981d543bb772af392d7
Carlos Perez <pcarlos132@yahoo.com>
2022-10-18 16:06:25 -0500

    october 18 2022

--- October 2022/main.js
+++ October 2022/main.js
@@ -130,3 +130,18 @@ var twoSum = function(nums, target) {
       }
   }
 };
+
+//October 18 2022
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+function sumMix(arr) {
+  let nums = arr.map(el => Number(el))
+  return nums.reduce((acc, el) => acc + el, 0)
+}
+
+console.log(sumMix([9, 3, '7', '3']))
+console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]))
+
+sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0'])
