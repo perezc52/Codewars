6d2b01c11b4b8b9324f6e0fff20efc3618bed964
Carlos Perez <pcarlos132@yahoo.com>
2022-10-17 21:52:26 -0500

    october 17 2022

--- October 2022/main.js
+++ October 2022/main.js
@@ -125,6 +125,7 @@ console.log(fizzbuzz(25))
 var twoSum = function(nums, target) {
   for(let i = 0; i < nums.length; i++) {
       for(let j = 0; j < nums.length; j++) {
+          if(i === j) continue
           if(nums[i] + nums[j] === target) return [i, j]
       }
   }
