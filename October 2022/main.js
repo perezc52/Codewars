c6f1e17534f81bee4ecd63a3108a51e950a6a27d
Carlos Perez <pcarlos132@yahoo.com>
2022-10-20 16:41:54 -0500

    october 20 2022

--- October 2022/main.js
+++ October 2022/main.js
@@ -164,3 +164,35 @@ function getAverage(arr) {
 console.log(getAverage([2,2,2,2]))
 console.log(getAverage([1,2,3,4,5,]))
 console.log(getAverage([1,1,1,1,1,1,1,2]))
+
+//October 20 2022
+// You will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+
+// For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
+
+// repeats([4,5,7,5,4,8]),15)
+// repeats([9, 10, 19, 13, 19, 13]),19)
+// repeats([16, 0, 11, 4, 8, 16, 0, 11]),12)
+// repeats([5, 17, 18, 11, 13, 18, 11, 13]),22)
+// repeats([5, 10, 19, 13, 10, 13]),24)
+
+function repeats(arr) {
+  let occurences = {}
+  
+  for(let i = 0; i < arr.length; i++) {
+    if(occurences[arr[i]]) {
+      occurences[arr[i]]++
+    }else {
+      occurences[arr[i]] = 1
+    }
+  }
+
+  return arr.filter(el => occurences[el] < 2).reduce((acc, el) => acc + el, 0)
+
+}
+
+console.log(repeats([4,5,7,5,4,8]),15)
+console.log(repeats([9, 10, 19, 13, 19, 13]),19)
+console.log(repeats([16, 0, 11, 4, 8, 16, 0, 11]),12)
+console.log(repeats([5, 17, 18, 11, 13, 18, 11, 13]),22)
+console.log(repeats([5, 10, 19, 13, 10, 13]),24)
