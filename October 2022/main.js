734b9bd8c123d8af69c2837d7751fb67a47254e0
Carlos Perez <pcarlos132@yahoo.com>
2022-10-25 16:41:57 -0500

    october 25 2022

--- October 2022/main.js
+++ October 2022/main.js
@@ -213,3 +213,21 @@ function evenNumbers(arr, num) {
 
 console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))
 console.log(evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2))
+
+//October 25 2022
+// Return the number of unique arrays that can be formed by picking exactly one element from each subarray.
+
+// For example: solve([[1,2],[4],[5,6]]) = 4, because it results in only 4 possibilites. They are [1,4,5],[1,4,6],[2,4,5],[2,4,6].
+
+// Make sure that you don't count duplicates; for example solve([[1,2],[4,4],[5,6,6]]) = 4, since the extra outcomes are just duplicates.
+
+// solve([[1,2],[4],[5,6]]),4)
+// solve([[1,2],[4,4],[5,6,6]]),4)
+// solve([[1,2],[3,4],[5,6]]),8)
+// solve([[1,2,3],[3,4,6,6,7],[8,9,10,12,5,6]]),72)
+
+function solve(arr) {
+  return arr.map(el => [...new Set(el)])
+            .map(el => el.length)
+            .reduce((acc, el) => acc * el)
+};
