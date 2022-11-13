5bddfaa1bff98348afbd17cf713addc50204af94
Carlos Perez <pcarlos132@yahoo.com>
2022-11-13 17:48:41 -0600

    november 13 2022

--- November 2022/main.js
+++ November 2022/main.js
@@ -11,6 +11,32 @@ function factorial(num) {
 
 console.log(factorial(5))
 
-//November 12 2022
+//November 13 2022
+// The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
 
-hey
+// The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+
+// If the score < 0, return 0.
+
+// For example:
+
+// checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+// checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+// checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+// checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+
+function checkExam(array1, array2) {
+    let score = 0
+    for(let i = 0; i < array1.length; i++) {
+      if(array1[i] === array2[i]) {
+        score += 4
+      }
+      if(array2[i] === "") {
+        continue
+      }
+      if(array1[i] !== array2[i]) {
+        score -= 1
+      }
+    }
+    return score < 0 ? 0 : score
+  }
