c026b912f6c75e02e9616ee7c82c5de431b5bf0f
Carlos Perez <pcarlos132@yahoo.com>
2023-01-02 19:31:31 -0600

    january 2 2023

--- January 2023/main.js
+++ January 2023/main.js
@@ -3,3 +3,14 @@
 function opposite(number) {
     return number * -1
   }
+
+//January 2
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+function repeatStr (n, s) {
+  return s.repeat(n)
+}
