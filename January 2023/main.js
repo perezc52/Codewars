e22ef292a30aa061a9eb098f2e04b5eeaa3f2ee6
Carlos Perez <pcarlos132@yahoo.com>
2023-01-03 19:57:43 -0600

    january 3 2023

--- January 2023/main.js
+++ January 2023/main.js
@@ -14,3 +14,17 @@ function opposite(number) {
 function repeatStr (n, s) {
   return s.repeat(n)
 }
+
+//January 3
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+function getCount(str) {
+  let count = 0
+  for(let i = 0; i < str.length; i++) {
+    if("aeiou".includes(str[i])) count++
+  }
+  return count
+}
