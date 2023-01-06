a47e8e8b05c98849b260916a6e808989f7809dc5
Carlos Perez <pcarlos132@yahoo.com>
2023-01-05 18:35:26 -0600

    january 5 2023

--- January 2023/main.js
+++ January 2023/main.js
@@ -40,3 +40,10 @@ function findNeedle(haystack) {
     if(haystack[i] === "needle") return `found the needle at position ${i}`
   }
 }
+
+//January 5 2023
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+export function even_or_odd(n:number):string {
+  return n % 2 === 0 ? "Even" : "Odd"
+}
