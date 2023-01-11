5a61acd93118ba3a1d63719c467aafa9b22d6461
Carlos Perez <pcarlos132@yahoo.com>
2023-01-10 19:32:15 -0600

    january 10 2023

--- January 2023/main.js
+++ January 2023/main.js
@@ -98,3 +98,23 @@ function points(games) {
 function reverseLetter(str) {
   return str.match(/[a-z]/gi).reverse().join('')
 }
+
+//January 10 2023
+// Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+// Example:
+
+// Input:
+
+// 'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+// Output:
+
+// 'alpha beta gamma delta'
+
+function removeDuplicateWords (s) {
+  let sArray = s.split(' ')
+  let sSet = [...new Set(sArray)]
+  return sSet.join(' ')
+}
+
