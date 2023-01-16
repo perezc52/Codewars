f09ff2c6a953c4d3740ab4f0fed4028b3f4ab3c5
Carlos Perez <pcarlos132@yahoo.com>
2023-01-15 19:42:01 -0600

    january 15 2023

--- January 2023/main.js
+++ January 2023/main.js
@@ -166,4 +166,19 @@ function vowelIndices(word){
   return vowels
 }
 
+//January 15 2023
+// Find the number with the most digits.
+
+// If two numbers in the argument array have the same number of digits, return the first one in the array.
+
+function findLongest(array){
+  array = array.map(el => String(el))
+  let longest = array[0]
+  for(let i = 0; i < array.length; i++) {
+    if(array[i].length > longest.length) longest = array[i]
+  }
+  return Number(longest)
+}
+
+
 
