a152c390269c6cd4b965bb8793426e67bbb40b98
Carlos Perez <pcarlos132@yahoo.com>
2023-01-17 18:39:53 -0600

    january 17 2023

--- January 2023/main.js
+++ January 2023/main.js
@@ -180,5 +180,20 @@ function findLongest(array){
   return Number(longest)
 }
 
+//January 17 2023
+// Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+// Example:
+
+// "riley" --> "Hello Riley!"
+// "JACK"  --> "Hello Jack!"
+
+var greet = function(name) {
+name = name.toLowerCase()
+return `Hello ${name[0].toUpperCase()}${name.slice(1)}!`
+};
+
+
+
 
 
