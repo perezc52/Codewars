5a94714376501e2b8e71822248d3471b0b3ebff8
Carlos Perez <pcarlos132@yahoo.com>
2023-01-18 18:27:43 -0600

    january 18 2023

--- January 2023/main.js
+++ January 2023/main.js
@@ -193,6 +193,21 @@ name = name.toLowerCase()
 return `Hello ${name[0].toUpperCase()}${name.slice(1)}!`
 };
 
+//January 18 2023
+// Sort the Gift Code
+
+// Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
+// Examples (Input -- => Output):
+
+// "abcdef"                      -- => "abcdef"
+// "pqksuvy"                     -- => "kpqsuvy"
+// "zyxwvutsrqponmlkjihgfedcba"  -- => "abcdefghijklmnopqrstuvwxyz"
+
+function sortGiftCode(code){
+  return code.split('').sort().join('')
+}
+
+
 
 
 
