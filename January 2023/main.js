28097bf4ceb872e215fc532c42b35b077fb504c8
Carlos Perez <pcarlos132@yahoo.com>
2023-01-20 19:30:14 -0600

    january 20 2023

--- January 2023/main.js
+++ January 2023/main.js
@@ -222,6 +222,16 @@ function toNumberArray(stringarray){
   return stringarray.map(el => Number(el))
 }
 
+//January 20 2023 Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+
+// Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n). If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
+
+function bump(x){
+  let bumps = x.split('').filter(el => el === 'n').length
+  return bumps <= 15 ? "Woohoo!" : "Car Dead"
+}
+
+
 
 
 
