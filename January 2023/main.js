472a4359966b5f4eace27af422aa68027dda1df6
Carlos Perez <pcarlos132@yahoo.com>
2023-01-22 19:33:47 -0600

    january 22 2023

--- January 2023/main.js
+++ January 2023/main.js
@@ -261,6 +261,17 @@ function addLetters(...letters) {
   return letter
 }
 
+//January 22 2023
+// JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+// The solution would work like the following:
+
+// getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+
+function getEvenNumbers(numbersArray){
+  return numbersArray.filter(el => el % 2 === 0)
+}
+
 
 
 
