586878ffa2c56d6ad442d4971873402b9577d38f
Carlos Perez <pcarlos132@yahoo.com>
2023-01-24 13:16:58 -0600

    january 24 2023

--- January 2023/main.js
+++ January 2023/main.js
@@ -295,6 +295,22 @@ function averages(numbers) {
   return averages
 }
 
+//January 24 2023
+// Given a mixed array of number and string representations of integers, add up the non-string integers and subtract this from the total of the string integers.
+
+// Return as a number.
+
+function divCon(x){
+  let stringSum = x
+                  .filter(el => typeof el === 'string')
+                  .map(el => Number(el))
+                  .reduce((acc, el) => acc + el, 0)
+  let integerSum = x
+                  .filter(el => typeof el === 'number')
+                  .reduce((acc, el) => acc + el, 0)
+  return integerSum - stringSum
+}
+
 
 
 
