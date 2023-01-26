1787ce0272987df738532ebaff0e17104b8ddb61
Carlos Perez <pcarlos132@yahoo.com>
2023-01-26 16:54:20 -0600

    january 26 2023

--- January 2023/main.js
+++ January 2023/main.js
@@ -335,6 +335,26 @@ function reverseNum(num) {
   return Number(num.toString().split('').reverse().join(''))
 }
 
+//January 26 2023
+// Task
+// Given a positive integer n, calculate the following sum:
+
+// n + n/2 + n/4 + n/8 + ...
+// All elements of the sum are the results of integer division.
+
+// Example
+// 25  =>  25 + 12 + 6 + 3 + 1 = 47
+
+function halvingSum(n) {
+  let sum = n
+  while(n >= 1) {
+    sum += Math.floor(n / 2)
+    n = Math.floor(n / 2)
+  }
+  return sum
+}
+
+
 
 
 
