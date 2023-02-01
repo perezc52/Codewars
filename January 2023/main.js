fc295d8fce8ab78b10b9811600f02ac946c0e959
Carlos Perez <pcarlos132@yahoo.com>
2023-01-31 20:05:08 -0600

    january 31 2023

--- January 2023/main.js
+++ January 2023/main.js
@@ -418,6 +418,16 @@ function squares(x, n) {
   return result
 }
 
+//January 31 2023
+// Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
+// Example(Input1, Input2 --> Output)
+
+// [1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
+
+function divisibleBy(numbers, divisor){
+  return numbers.filter(el => el % divisor === 0)
+}
+
 
 
 
