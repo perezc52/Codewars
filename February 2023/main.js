200a9ebb4c2da33fbdc47940f077103db72e21cd
Carlos Perez <pcarlos132@yahoo.com>
2023-02-04 15:09:14 -0600

    february 4 2023

--- February 2023/main.js
+++ February 2023/main.js
@@ -41,3 +41,56 @@ function capitalize(s){
   let two = s.split('').map((el,i) => i % 2 !== 0 ? el.toUpperCase() : el).join('')
   return [one, two]
 };
+
+//February 4 2023
+// There is a war and nobody knows - the alphabet war!
+// There are two groups of hostile letters. The tension between left side letters and right side letters was too high and the war began.
+// Task
+
+// Write a function that accepts fight string consists of only small letters and return who wins the fight. When the left side wins return Left side wins!, when the right side wins return Right side wins!, in other case return Let's fight again!.
+
+// The left side letters and their power:
+
+//  w - 4
+//  p - 3
+//  b - 2
+//  s - 1
+
+// The right side letters and their power:
+
+//  m - 4
+//  q - 3
+//  d - 2
+//  z - 1
+
+function alphabetWar(fight) {
+  const left = {
+    w: 4,
+    p: 3,
+    b: 2,
+    s: 1,
+  }
+  const right = {
+    m: 4,
+    q: 3,
+    d: 2,
+    z: 1,
+  }
+  
+  let rightSum = 0
+  let leftSum = 0
+  
+  for(let i = 0; i < fight.length; i++) {
+    if(left[fight[i]]) leftSum += left[fight[i]]
+    if(right[fight[i]]) rightSum += right[fight[i]]
+  }
+  
+  if(rightSum > leftSum) {
+    return 'Right side wins!'
+  }else if(leftSum > rightSum) {
+    return 'Left side wins!'
+  }else {
+    return "Let's fight again!"
+  }
+}
+
