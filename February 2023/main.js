5475bc26b74230425ed60cfb941e448f00282c8e
Carlos Perez <pcarlos132@yahoo.com>
2023-02-06 16:18:11 -0600

    february 6 2023

--- February 2023/main.js
+++ February 2023/main.js
@@ -106,3 +106,16 @@ function hoopCount (n) {
   return n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it"
 }
 
+//February 6 2023
+// Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.
+
+// @ @@ @ @@ @ @@ @ @@ @ @@ @
+
+// Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, TypeScript, C, C++ countRedBeads(n)) so that it returns the number of red beads.
+// If there are less than 2 blue beads return 0.
+
+function countRedBeads(n) {
+  if(n < 2) return 0
+  return (n - 1) * 2
+}
+
