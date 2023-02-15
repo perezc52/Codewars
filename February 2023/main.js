e7e603e7654b3656535c8f99b9d3c6375508b2f9
Carlos Perez <pcarlos132@yahoo.com>
2023-02-14 19:41:21 -0600

    february 14 2023

--- February 2023/main.js
+++ February 2023/main.js
@@ -232,3 +232,23 @@ function factorial(num) {
   }
     return res
 }
+
+//February 14 2023
+// Grade book
+
+// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+// Numerical Score 	Letter Grade
+// 90 <= score <= 100 	'A'
+// 80 <= score < 90 	'B'
+// 70 <= score < 80 	'C'
+// 60 <= score < 70 	'D'
+// 0 <= score < 60 	'F'
+
+function getGrade (s1, s2, s3) {
+  let avg = (s1 + s2 + s3) / 3
+  if(avg >= 90 && avg <= 100) return 'A'
+  if(avg >= 80 && avg < 90) return 'B'
+  if(avg >= 70 && avg < 80) return 'C'
+  if(avg >= 60 && avg < 70) return 'D'
+  if(avg < 60) return 'F'
+}
