11714f5e40b7fc2f0d817368371e8678ee5eb8f2
Carlos Perez <pcarlos132@yahoo.com>
2023-02-17 17:21:21 -0600

    february 17 2023

--- February 2023/main.js
+++ February 2023/main.js
@@ -279,3 +279,22 @@ function correct(string)
     return el
   }).join('')
 }
+
+//February 17 2023
+// Given an input of an array of digits, return the array with each digit incremented by its position in the array: the first digit will be incremented by 1, the second digit by 2, etc. Make sure to start counting your positions from 1 ( and not 0 ).
+
+// Your result can only contain single digit numbers, so if adding a digit with its position gives you a multiple-digit number, only the last digit of the number should be returned.
+// Notes:
+
+//     return an empty array if your array is empty
+//     arrays will only contain numbers so don't worry about checking that
+
+function incrementer(nums) { 
+  return nums.map((el,i) => {
+    let num = el + (i + 1)
+    if(num >= 10) {
+      num = Number(num.toString().slice(1))
+    }
+    return num
+  })
+}
