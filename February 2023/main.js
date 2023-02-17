a4ace5877ce216e0d1027fd2aa6c0d33b041c76e
Carlos Perez <pcarlos132@yahoo.com>
2023-02-16 20:09:45 -0600

    february 16 2023

--- February 2023/main.js
+++ February 2023/main.js
@@ -260,3 +260,22 @@ function getGrade (s1, s2, s3) {
 function problem(x){
   return typeof x === 'string' ? "Error" : (x * 50) + 6
 }
+
+//February 16 2023
+// Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+//     S is misinterpreted as 5
+//     O is misinterpreted as 0
+//     I is misinterpreted as 1
+
+// The test cases contain numbers only by mistake.
+
+function correct(string)
+{
+	return string.split('').map(el => {
+    if(el === '5') return 'S'
+    if(el === '0') return 'O'
+    if(el === '1') return 'I'
+    return el
+  }).join('')
+}
