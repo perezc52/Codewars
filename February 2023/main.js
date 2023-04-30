27d5d5f2e9026af523f6b43c142b19c4050f6055
Carlos Perez <pcarlos132@yahoo.com>
2023-04-30 18:24:00 -0500

    april 30 2023

--- February 2023/main.js
+++ February 2023/main.js
@@ -297,4 +297,16 @@ function incrementer(nums) {
     }
     return num
   })
-}jh
+}
+
+// Complete the function that takes a sequence of numbers as single parameter. Your function must return the sum of the even values of this sequence.
+
+// Only numbers without decimals like 4 or 4.0 can be even.
+
+// The input is a sequence of numbers: integers and/or floats. 
+
+function sumEvenNumbers(input) {
+  let evens = input.filter(el => el % 2 === 0)
+  let sumOfEvens = evens.reduce((acc, el) => acc + el, 0)
+  return sumOfEvens
+}
