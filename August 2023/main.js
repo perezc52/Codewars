b6f2a7f1dcfe7dd46a261d0d5ff71846bedf7726
Carlos Perez <pcarlos132@yahoo.com>
2023-08-01 20:00:14 -0500

    august 1 2023

--- August 2023/main.js
+++ August 2023/main.js
@@ -24,3 +24,15 @@ function inviteMoreWomen(L) {
     let men = L.filter(el => el === 1).length
     return women >= men ? false : true
    }
+
+   //August 1 2023
+
+//    You're at the zoo... all the meerkats look weird. Something has gone terribly wrong - someone has gone and switched their heads and tails around!
+
+// Save the animals by switching them back. You will be given an array which will have three values (tail, body, head). It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).
+
+// Same goes for all the other arrays/lists that you will get in the tests: you have to change the element positions with the same exact logics
+
+function fixTheMeerkat(arr) {
+ return arr.reverse()
+}
