dcef1136eaebf2c30edadbfe86b79905fc00b71c
Carlos Perez <pcarlos132@yahoo.com>
2023-08-02 20:16:48 -0500

    august 2 2023

--- August 2023/main.js
+++ August 2023/main.js
@@ -36,3 +36,10 @@ function inviteMoreWomen(L) {
 function fixTheMeerkat(arr) {
  return arr.reverse()
 }
+
+//August 2 2023
+// Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value. 
+
+function explode(s) {
+  return s.split('').map(el => el.repeat(Number(el))).join('')
+}
