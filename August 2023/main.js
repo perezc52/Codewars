8fcfc8e763e107d42507f07382f7e99c026c2b75
Carlos Perez <pcarlos132@yahoo.com>
2023-08-11 14:44:42 -0500

    august 11 2023

--- August 2023/main.js
+++ August 2023/main.js
@@ -184,5 +184,25 @@ var flatten = function (array){
   return array.flat()
 }
 
+//August 11 2023
+// Let's see some cases (input -> output):
+
+// 1, 10  --> [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// 1, 100 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+
+// If there are no numbers of this kind in the range [a,b][a, b][a,b] the function should output an empty list.
+
+// 90, 100 --> []
+
+function sumDigPow(a, b) {
+  let res = []
+  for(let i = a; i <= b; i++) {
+    let output = String(i).split('').map((el,i) => Math.pow(Number(el), i+1)).reduce((acc,el) => acc + el,0)
+    if(output == i) res.push(i)
+  }
+  return res
+}
+
+
 
 
