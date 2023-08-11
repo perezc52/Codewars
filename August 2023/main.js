b2899eb0b429045df9f58d89971fa51e6ff4684c
Carlos Perez <pcarlos132@yahoo.com>
2023-08-10 19:43:21 -0500

    august 10 2023

--- August 2023/main.js
+++ August 2023/main.js
@@ -173,4 +173,16 @@ class Dinglemouse{
   
 }
 
+//August 10 2023
+// Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
+
+// flatten([1,2,3]) // => [1,2,3]
+// flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
+// flatten([[[1,2,3]]]) // => [[1,2,3]]
+
+var flatten = function (array){
+  return array.flat()
+}
+
+
 
